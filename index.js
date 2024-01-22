dc3b54eb560227f5ce5428396e523ee6340a5e27
Nemobraga <neemiassantos217@gmail.com>
2024-01-21 23:36:54 -0300

    instalanmdo node

---
+++ index.js
@@ -0,0 +1,28 @@
+import ler from 'readline-sync';
+
+
+
+let hero = ler.question('Digete o nome do Heroi:');
+while(hero != null) {
+     hero = ler.question('Digete o nome do Heroi:');
+
+    let  xp = ler.question('Digete o XP do Heroi:');
+
+    for(xp)
+            
+          xp = ler.question('Digete o XP do Heroi:');
+    
+
+    if(xp <1000){
+        let nivel ="ferro";
+    };
+
+
+};
+
+
+
+
+
+
+
